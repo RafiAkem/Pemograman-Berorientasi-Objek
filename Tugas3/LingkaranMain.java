95499396d6092a17d06a713298e015577c103ea8
RafiAkem <yc66zio@gmail.com>
2024-03-01 18:58:31 +0700

    Update

--- Tugas3/LingkaranMain.java
+++ Tugas3/LingkaranMain.java
@@ -2,15 +2,21 @@ package Tugas3;
 
 public class LingkaranMain {
     public static void main(String[] args) {
+        // Buat objek l1 dengan jari-jari 5
         Lingkaran lingkaran1 = new Lingkaran(5);
+        // Buat objek l2 dengan jari-jari sama dengan l1
         Lingkaran lingkaran2 = lingkaran1;
+        // Cetak jari-jari l1, l2
         System.out.println(lingkaran1.getJari2());
         System.out.println(lingkaran2.getJari2());
+        // Set l2 menjadi 4
         ubahJari2(lingkaran2);
+        // Cetak jari-jari l1, l2
         System.out.println(lingkaran1.getJari2());
         System.out.println(lingkaran2.getJari2());
     }
 
+    // Buat method ubahJari2
     public static void ubahJari2(Lingkaran obj) {
         obj.setJari2(4);
     }
@@ -18,15 +24,15 @@ public class LingkaranMain {
 
 class Lingkaran {
     private double jari2;
-
+    // Buat constructor Lingkaran
     public Lingkaran(double jari2) {
         this.jari2 = jari2;
     }
-
+    // Buat method getJari2
     public double getJari2() {
         return jari2;
     }
-
+    // Buat method setJari2
     public void setJari2(double jari2) {
         this.jari2 = jari2;
     }
--- Tugas3/RefObjek2.java
+++ Tugas3/RefObjek2.java
@@ -1,14 +1,21 @@
 package Tugas3;
 
+
 public class RefObjek2 {
     public static void main(String[] args) {
+        // Buat objek l1 dengan jari-jari 5
         Lingkaran l1 = new Lingkaran(5);
+        // Buat objek l2 dengan jari-jari sama dengan l1
         Lingkaran l2 = l1;
+        // Buat objek l3 dengan jari-jari 7
         Lingkaran l3 = new Lingkaran(7);
+        // Cetak jari-jari l1, l2, dan l3   
         System.out.println(l1.getJari2());
         System.out.println(l2.getJari2());
         System.out.println(l3.getJari2());
+        // Set l2 menjadi l3
         l2 = l3;
+        // Cetak jari-jari l1, l2, dan l3
         System.out.println(l1.getJari2());
         System.out.println(l2.getJari2());
         System.out.println(l3.getJari2());
